988265ba4e2796e89df9ef87bf515eed364b3a94
Jorge-Ortiz-M <jorge.ortiz@icalialabs.com>
2022-06-10 16:22:50 -0500

    Adding the post methods to the User model. Creating of the application DB with Mongoose, as well as the User model. Also, we added the findOne user and saveUser inside of the Post methods.

---
+++ README.md
@@ -0,0 +1,32 @@
+# Authentication Project.
+
+
+## Node packages.
+
+If you want to run this web application in local, you should run and install this packages.
+
+1. Initialize NPM: npm init.
+2. Install Express JS: npm install ejs.
+3. Install Body-Parser: npm install body-parser.
+4. Install Nodemon: npm install nodemon.
+5. Install Mongoose: npm install mongoose.
+6. Install Mongoose-Encryption: npm install mongoose-encryption.
+
+## Web application.
+
+In the section below, you can find images about this web aaplication in production.
+
+![](images/daily-home.png).
+
+
+![](images/daily-app01.png).
+
+
+![](images/daily-app012.png).
+
+## Author
+
+* Jorge Ortiz
+* jorge.ortiz@icalialabs.com
+* Software engineer.
+* San Luis Potosí, S.L.P. México.
--- app.js
+++ app.js
@@ -3,12 +3,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+
+mongoose.connect("mongodb://localhost:27017/usersDB", { useNewUrlParser: true });
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
+
+// -------------- GET ---------------
 
 app.get("/", (req, res) => {
     res.render('home');
@@ -16,11 +28,45 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
     res.render('login');
-})
+});
 
 app.get("/register", (req, res) => {
     res.render('register');
-})
+});
+
+// -------------- POST ---------------
+
+app.post("/register", (req, res) => {
+    const user = new User ({
+        email: req.body.username,
+        password: req.body.password
+    });
+    user.save(function(err){
+        if(err){
+            console.log(err);
+        } else {
+            res.render('secrets');
+        }
+    });
+});
+
+app.post("/login", (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+    User.findOne({email: email}, function(err, userFounded){
+        if(err){
+            console.log(err);
+        } else {
+            if(userFounded.password === password){
+                res.render('secrets');
+            } else {
+                res.redirect("/login");
+            }
+        }
+    });
+});
+
+// -------------- PORTS ---------------
 
 app.listen(port, () => {
     console.log("Listening on port 3000");
